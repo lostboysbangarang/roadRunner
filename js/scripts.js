e05195da99eb1d869e479d0f21a333314db4c302
Hayden Lugo <hayden@lugowebdevelopment.com>
2021-09-11 16:55:41 -0500

    Day 1

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,17 @@ window.onload=startFunc()
 function startFunc() {
     console.log("help")
     const wheelI=document.getElementsByClassName("rollOut_wheel_1");
-    wheelI[0].style.animation="roll 20000ms ease-in forwards";
+    // wheelI[0].style.animation="roll 20000ms 1500ms ease-in forwards";
+    $(".wheel").css("animation", "roll 20000ms 2000ms ease-in forwards");
+    $(".rollOut").css("animation", "drive 4000ms 2000ms ease-in forwards");
+    $(".rollOut").on("animationend webkitAnimationEnd oAnimationEnd", function () {
+        $(".rollOut").remove();
+    })
+    // $(".buttons > input").each(function() {
+    //     console.log(this);
+    //     this.style.width = "fit-content";
+    //     this.style.padding = "0vw 1vw"
+    //     this.innerText = this.classList.vlaue
+    // });
+    console.log($(".buttons")[0].children);
 }
